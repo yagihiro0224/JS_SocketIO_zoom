83dca96b8481e37f134eae5a277381b1728fcc6b
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-30 03:29:54 +0900

    commit.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
-// alert("Hi~");
+// 여기의 socket은 서버로의 연결을 뜻한다.
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -10,12 +10,19 @@ app.use("/public", experss.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on wss://localhost:3000");
+const handleListen = () => console.log("Listening on http://localhost:3000");
 
 // 같은 서버에서 http server, ws server 둘다 돌리기 위한 코드.
 // http가 필요없을 경우엔 ws만 코딩하면 됨.
 // 두 놈이 같은 포트(3000)에 있게 하고 싶어서 이렇게 만듦.
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// socket 너와 브라우저와의 연결이다.
+function handleConnection(socket) {
+  console.log(socket);
+}
+// on 메소드는 이벤트가 발생하길 기다림. 발생하면 해당 함수를 실행함.
+wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
