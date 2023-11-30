de67ff9b52821035cb910ed499329a7e51dcae1a
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-11-30 23:38:38 +0900

    before new start.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
 import experss, { application } from "express";
 // commit test
 const app = experss();
@@ -15,8 +16,9 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 // 같은 서버에서 http server, ws server 둘다 돌리기 위한 코드.
 // http가 필요없을 경우엔 ws만 코딩하면 됨.
 // 두 놈이 같은 포트(3000)에 있게 하고 싶어서 이렇게 만듦.
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wsServer = SocketIO(httpServer);
 
 // const sockets = [];
 
@@ -47,4 +49,4 @@ const wss = new WebSocket.Server({ server });
 //   });
 // });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
