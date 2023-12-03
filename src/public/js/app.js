cb776495453de49dad18e86f8e6dbf4e46640679
yagi.hiro.0224 <yagi.hiro.0224@gmail.com>
2023-12-03 23:51:20 +0900

    commit.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,47 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMeida() {
+  try {
+    if (navigator.mediaDevices) {
+      myStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      myFace.srcObject = myStream;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMeida();
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import http from "http";
+
+import SocketIO from "socket.io";
 import experss, { application } from "express";
+import { instrument } from "@socket.io/admin-ui";
+import { Socket } from "dgram";
 
 const app = experss();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", experss.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = new SocketIO(httpServer);
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
-// Adapter가 하는 일은 다른 서버들 사이에 실시간 어플리케이션을 동기화 하는것.
